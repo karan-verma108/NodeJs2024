87e35415be42e655d0ac4f04baac3942569ca41d
karan <karanverma201411@gmail.com>
2024-02-10 23:30:33 +0530

    CRUD operations

---
+++ challenge/index.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+
+// fs.mkdirSync('./thapa', {recursive: true}, (err)=>{
+//     if(err){
+//         console.error('folder not created');
+//     }else{
+//         console.log('folder created');
+//     }
+// })
+
+// fs.mkdirSync('newthapa');
+// fs.rmdirSync('newthapa');
+
+// fs.writeFileSync('./thapa/newfile.txt', 'i created this file simply');
+
+// fs.appendFileSync('./thapa/newfile.txt', ' ya this is more data added to the same file');
+
+// const fileData = fs.readFileSync('./thapa/newfile.txt', 'utf-8');
+// console.log(fileData);
+
+// fs.renameSync('./thapa/newfile.txt', './thapa/oldfile.txt');
+
+// fs.unlink('./thapa/oldfile.txt', (err)=>{
+//     if(err){
+//         console.error('file not removed');
+//     }else{
+//         console.log('file removed');
+//     }
+// });
+
+// fs.writeFileSync('./thapa/newfile.txt', "hi guys");
+
+// fs.unlinkSync('./thapa/newfile.txt');
+
+// fs.rmdirSync('./thapa');
+// fs.mkdirSync('thapa');
+
+fs.rmdir('./thapa', (err)=>{
+    if(err){
+        console.error('folder not removed');
+    }else{
+        console.log('folder removed');
+    }
+});
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const path = require('path');
 
 // writing/creating a new file using fs module 
 // fs.writeFileSync('read.txt', "hi this is a new file created using file system module in node.js");
@@ -17,4 +18,42 @@ const fs = require('fs');
 // fs.renameSync('read.txt', 'readWrite.txt');
 
 // deleting a file 
-fs.unlinkSync('readWrite.txt');
+// fs.unlinkSync('readWrite.txt');
+
+/* creating a new folder 
+ fs.mkdir('./new-folder', { recursive: true }, (err) => {
+     if (err) {
+         console.error('error creating the folder', err);
+     } else {
+         console.log('folder created succesfull!');
+     }
+ });*/
+
+/*writing a new file inside of a folder
+ const filePath = path.join('./new-folder', 'new-file.txt');
+ fs.writeFileSync(filePath, "hello world", (err) => {
+     if (err){
+         console.error('error creating file', err);
+     } else{
+         console.log('new file created');
+     }
+ })*/
+
+/* updating a file inside of a folder 
+fs.appendFileSync('./new-folder/new-file.txt', ' more data added');
+
+// reading a file inside of a folder
+const fileContent = fs.readFileSync('./new-folder/new-file.txt', "utf-8");
+console.log(fileContent); */
+
+/* renaming a file inside of a folder 
+ fs.renameSync('./new-folder/new-file.txt', './new-folder/hifile.txt');*/
+
+//to delete a folder/directory
+// try {
+//     fs.rmdirSync('./new-folder');
+//     console.log('directory removed');
+// } catch (error) {
+//     console.error('error deleting the file', error);
+// }
+
