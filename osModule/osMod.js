6bf437066af2f8f859505074c4c7ada0647e8dac
karan <karanverma201411@gmail.com>
2024-02-16 12:31:05 +0530

    os module seen

---
+++ osModule/osMod.js
@@ -0,0 +1,18 @@
+const os = require('os');
+
+/*const totalMemory = (os.totalmem()/1024/1024/1024).toFixed(2);
+console.log(`Total memory in my system is ${totalMemory} GB`);
+
+const freeMemory = (os.freemem()/1024/1024/1024).toFixed(2);
+console.log(`Available memory in my system is ${freeMemory} GB`); */
+
+// let hostname = os.hostname();
+// console.log(hostname);
+
+// let platform = os.platform();
+// console.log(platform);
+
+// let architecture = os.arch();
+// console.log(architecture);
+
+console.log(os.type());
