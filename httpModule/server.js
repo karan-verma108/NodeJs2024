38a6fa1217dec1ef2f930a09348970d79f2f7af1
karan <karanverma201411@gmail.com>
2024-02-24 23:29:16 +0530

    used url module directly

--- httpModule/server.js
+++ httpModule/server.js
@@ -4,7 +4,17 @@ let port = '8000';
 // let's call the create server method using http module 
 
 let server = http.createServer((req, res) => {
-    res.end('Hello! I created this nodejs server without the need of expressjs so it is basically the very first time i created a web sever using solely nodejs. Earlier i used to think that we can only create web sever in nodejs using expressjs only but i was wront. Im glad that i was wrong and learned a new thing today. :)')
+    console.log(req.url);
+    if (req.url == '/') {
+        res.end('Hello! I created this nodejs server without the need of expressjs so it is basically the very first time i created a web sever using solely nodejs. Earlier i used to think that we can only create web sever in nodejs using expressjs only but i was wront. Im glad that i was wrong and learned a new thing today. :)')
+    } else if (req.url == '/about') {
+        res.end('Hi this is an about page');
+    } else if (req.url == '/contact') {
+        res.end('HI this is a contact page');
+    } else {
+        res.writeHead(404, { 'Content-type': 'text/html' });
+        res.end('<h1>404 page not found !</h1>');
+    }
 });
 
 server.listen(port, () => {
