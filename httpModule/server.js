4f71bbf0cce5b6e0aa10f9fbb784f1ee38a0e633
karan <karanverma201411@gmail.com>
2024-02-26 09:44:55 +0530

    api calling

--- httpModule/server.js
+++ httpModule/server.js
@@ -1,22 +1,22 @@
 // let's first require the http module
 const http = require('http');
+const fs = require('fs');
 let port = '8000';
 // let's call the create server method using http module 
 
-let server = http.createServer((req, res) => {
-    console.log(req.url);
+const server = http.createServer((req, res) => {
+    const data = fs.readFileSync(`${__dirname}/userAPI/userApi.json`, 'utf8');  //this will call the api only once
+
     if (req.url == '/') {
-        res.end('Hello! I created this nodejs server without the need of expressjs so it is basically the very first time i created a web sever using solely nodejs. Earlier i used to think that we can only create web sever in nodejs using expressjs only but i was wront. Im glad that i was wrong and learned a new thing today. :)')
+        res.end('welcome to homepage');
     } else if (req.url == '/about') {
-        res.end('Hi this is an about page');
-    } else if (req.url == '/contact') {
-        res.end('HI this is a contact page');
-    } else {
-        res.writeHead(404, { 'Content-type': 'text/html' });
-        res.end('<h1>404 page not found !</h1>');
+        res.end('welcome to about page');
+    } else if (req.url == '/userapi') {
+        res.writeHead(200, { 'Content-type': 'application/json' });
+        res.end(data);
     }
 });
 
 server.listen(port, () => {
-    console.log(`The nodejs server is listening at port no. ${port}`);
+    console.log(`nodejs server is running at port ${port}`);
 });
